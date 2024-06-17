f3b47912addfd1d0e17d196264ba5c12a566cd48
Viviorigi <duong682@gmail.com>
2024-06-17 11:23:17 +0700

    phan quyen

--- shopapp/src/main/java/com/project/shopapp/components/JwtTokenUtil.java
+++ shopapp/src/main/java/com/project/shopapp/components/JwtTokenUtil.java
@@ -8,6 +8,7 @@ import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.InvalidParameterException;
@@ -59,7 +60,7 @@ public class JwtTokenUtil {
         return Jwts.parserBuilder()
                 .setSigningKey(getSignInKey())
                 .build()
-                .parseClaimsJwt(token)
+                .parseClaimsJws(token)
                 .getBody();
     }
     public<T> T extractClaim(String token, Function<Claims, T> claimsResolver){
@@ -72,5 +73,11 @@ public class JwtTokenUtil {
     }
     public String extractPhoneNumber(String token){
         return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean validatetoken(String token, UserDetails userDetails){
+        String phoneNumber = extractPhoneNumber(token);
+        return (phoneNumber.equals(userDetails.getUsername()))
+                && !isTokenExpire(token);
     }
 }
